75d2fb7d6ba08f39b87db1812e17efe9ad564c0d
peder2911 <pglandsverk@gmail.com>
2021-05-27 16:57:12 +0200

    Finished configuration management

--- viewser/cli.py
+++ viewser/cli.py
@@ -1,9 +1,10 @@
 
-from datetime import date,datetime
+from datetime import datetime
 from typing import Optional
 import io
 import click
 import tabulate
+from . import crud, settings
 
 @click.group()
 def viewser():
@@ -79,7 +80,7 @@ def queryset_show(name: str, json: bool):
     click.echo(f"Queryset {name}...")
 
 @queryset.command(name="delete", short_help="delete a queryset")
-@click.confirmation_option(prompt="Are you sure?")
+@click.confirmation_option(prompt="Delete queryset?")
 @click.argument("name", type=str)
 def queryset_delete(name: str):
     """
@@ -87,37 +88,63 @@ def queryset_delete(name: str):
     """
     click.echo(f"Deleting Queryset {name}")
 
-@viewser.group(name="configure", short_help="configure viewser")
-def configure():
+@viewser.group(name="config", short_help="configure viewser")
+def config():
     """
     Configure viewser
     """
-    pass
 
-@configure.command(name="interactive", short_help="interactively configure viewser")
+@config.command(name="interactive", short_help="interactively configure viewser")
 def config_interactive():
     """
     Interactively configure viewser (useful for first-time config)
     """
-    click.echo("Configuring...")
+    settings.configure_interactively()
+    click.echo("All done!")
 
-@configure.command(name="set", short_help="set a configuration value")
+@config.command(name="set", short_help="set a configuration value")
 @click.argument("name", type=str)
 @click.argument("value", type=str)
 @click.option("--override/--no-override",default=True)
 def config_set(name: str, value: str, override: bool): # pylint: disable=redefined-builtin
     """
-    Set a configuration value
+    Set a configuration value.
     """
-    if override:
-        click.echo("Overriding existing value")
 
-    click.echo(f"Setting {name} to {value}")
+    overrides = True
+    try:
+        settings.config_get(name)
+    except KeyError:
+        overrides = False
 
-@configure.command(name="get", short_help="get a configuration value")
+    if not override and overrides:
+        click.echo(f"Setting {name} already set, override not specified (see --help)")
+        return
+
+    settings.config_set_in_file(name,value)
+    click.echo(f"{name}: {value}")
+
+@config.command(name="get", short_help="get a configuration value")
 @click.argument("name", type=str)
 def config_get(name: str):
     """
-    Get a configuration value
+    Get a configuration value.
     """
-    click.echo(f"Getting config value {name}")
+    click.echo(f"{name}: {settings.config_get(name)}")
+
+@config.command(name="reset", short_help="reset default config values")
+@click.confirmation_option(prompt="Reset config?")
+def config_reset():
+    """
+    Reset config, writing default values over current values.
+    """
+    settings.reset_config_file_defaults()
+    click.echo("Config file reset")
+
+@config.command(name="list", short_help="show all configuration settings")
+def config_list():
+    """
+    Show all current configuration values
+    """
+    click.echo(tabulate.tabulate(settings.config_dict.items()))
+
--- viewser/settings.py
+++ viewser/settings.py
@@ -4,6 +4,8 @@ import json
 import crayons
 import fitin
 
+from toolz.functoolz import compose,curry
+
 logger = logging.getLogger(__name__)
 
 REQUIRED_SETTINGS = (
@@ -15,28 +17,74 @@ DEFAULT_SETTINGS = {
         "RETRIES": 80,
     }
 
-config_dir = os.path.expanduser("~/.views")
-settings_file_path = os.path.join(config_dir,"config.json")
- 
+CONFIG_DIR = os.path.expanduser("~/.views")
+SETTINGS_FILE_PATH = os.path.join(CONFIG_DIR, "config.json")
+
+def load_config_from_file():
+    with open(SETTINGS_FILE_PATH) as f:
+        return json.load(f)
+
+def save_config_to_file(cfg):
+    with open(SETTINGS_FILE_PATH,"w") as f:
+        json.dump(cfg,f)
+
+def with_key_value(key,value,cfg):
+    cfg[key] = value
+    return cfg
+
+config_set = lambda load,save,key,value: compose(
+        save,
+        curry(with_key_value,key,value),
+        load,
+        )()
+
+config_set_in_file = curry(
+        config_set,
+        load_config_from_file,
+        save_config_to_file
+        )
+
+def reset_defaults(load,save):
+    with_defaults = [curry(with_key_value,k,v) for k,v in DEFAULT_SETTINGS.items()]
+    compose(
+            save,
+            compose(*with_defaults),
+            load,
+        )()
+
+reset_config_file_defaults = curry(
+        reset_defaults,
+        load_config_from_file,
+        save_config_to_file
+        )
+
 try:
-    try:
-        os.makedirs(config_dir)
-    except FileExistsError:
-        pass
+    os.makedirs(CONFIG_DIR)
+except FileExistsError:
+    pass
 
-    with open(settings_file_path) as f:
-        config_dict = json.load(f)
+try:
+    with open(SETTINGS_FILE_PATH) as settings_file:
+        config_dict = json.load(settings_file)
 except FileNotFoundError:
-    logger.critical("Could not find config file, please run: 'viewser configure'")
+    logger.critical("Could not find config file, please run: 'viewser config interactive'")
     config_dict = {}
+    save_config_to_file(config_dict)
 
-config = fitin.seek_config([
+config_get = fitin.seek_config([
     fitin.environs_resolver(),
     fitin.dict_resolver(config_dict),
     fitin.dict_resolver(DEFAULT_SETTINGS)
     ])
 
 try:
-    logging.basicConfig(level=getattr(logging,config("LOG_LEVEL")))
+    logging.basicConfig(level=getattr(logging,config_get("LOG_LEVEL")))
 except(AttributeError, KeyError):
     logging.basicConfig(level=logging.INFO)
+
+def configure_interactively():
+    for key in REQUIRED_SETTINGS:
+        config_set_in_file(
+                key,
+                input(f"{key} >> "),
+            )
