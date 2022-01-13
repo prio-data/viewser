0d1ee84592dbd3d075132e3fa2b3d27456dd2666
peder2911 <pglandsverk@gmail.com>
2022-01-13 15:25:39 +0100

    Fixed wrong path for storage driver.

--- viewser/storage/model_object.py
+++ viewser/storage/model_object.py
@@ -30,7 +30,7 @@ class ModelMetadataStorage(key_value_store.KeyValueStore):
                     user          = settings.config_get("MODEL_OBJECT_SFTP_USER"),
                     key_db_host   = settings.config_get("MODEL_OBJECT_KEY_DB_HOSTNAME"),
                     key_db_dbname = settings.config_get("MODEL_OBJECT_KEY_DB_DBNAME"),
-                    folder        = "data/metadata"),
+                    folder        = "data/model"),
                 serializer = json.Json()
                 )
 
