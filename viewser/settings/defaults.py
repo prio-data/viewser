816880eb986b9c9d50d455e31131a6af65768a7c
peder2911 <pglandsverk@gmail.com>
2022-01-20 14:42:45 +0100

    Fixed wrong error dump directory location

--- viewser/settings/defaults.py
+++ viewser/settings/defaults.py
@@ -1,7 +1,8 @@
+import os
 from viewser import error_handling
-from . import config
+from . import config, static
 
 def default_error_handler():
     return error_handling.ErrorDumper([
-        error_handling.FileErrorHandler(config.get("ERROR_DUMP_DIRECTORY")),
+        error_handling.FileErrorHandler(os.path.join(static.CONFIG_DIR, config.get("ERROR_DUMP_DIRECTORY"))),
         error_handling.StreamHandler()])
